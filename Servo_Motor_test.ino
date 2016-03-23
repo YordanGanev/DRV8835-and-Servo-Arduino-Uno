79e4428d7c62fc6af76496b86b63443622983f80
YordanGanev <yordanganew@gmail.com>
2016-03-24 00:58:17 +0200

    Added files via upload

---
+++ Servo_Motor_test.ino
@@ -0,0 +1,53 @@
+#include "pin_definitions.h"
+#include <Servo.h>
+
+Servo myServo;
+
+#define SPEED 140
+
+void setup() {
+  
+  pinMode(LEFT_MOTOR_DIR , OUTPUT);
+  pinMode(RIGHT_MOTOR_DIR, OUTPUT);
+  pinMode(LEFT_MOTOR_PWM, OUTPUT);
+  pinMode(RIGHT_MOTOR_PWM, OUTPUT);
+  
+  foward();
+  delay(2000);
+  backward();
+  delay(2000);
+  
+  myServo.attach(SERVO);
+  myServo.write(90);
+}
+
+void loop() {
+  foward();
+  for(int i = 0; i < 180; i += 10)
+  {
+    myServo.write(i);
+    delay(100);
+  }
+  backward();
+  for(int i = 180; i > 0; i -= 10)
+  {
+    myServo.write(i);
+    delay(100);
+  }
+}
+
+void foward()
+{
+  digitalWrite(LEFT_MOTOR_DIR, HIGH);
+  digitalWrite(RIGHT_MOTOR_DIR, HIGH);
+  analogWrite(LEFT_MOTOR_PWM, SPEED);
+  analogWrite(RIGHT_MOTOR_PWM, SPEED);
+}
+
+void backward()
+{
+  digitalWrite(LEFT_MOTOR_DIR, LOW);
+  digitalWrite(RIGHT_MOTOR_DIR, LOW);
+  analogWrite(LEFT_MOTOR_PWM, SPEED);
+  analogWrite(RIGHT_MOTOR_PWM, SPEED);
+}
---
+++ pin_definitions.h
@@ -0,0 +1,20 @@
+//Motors' Definitions
+#define LEFT_MOTOR_DIR	          PB0  //8
+#define RIGHT_MOTOR_DIR 	        PD7  //7
+#define LEFT_MOTOR_PWM		        PB2  //10
+#define RIGHT_MOTOR_PWM		        PB1  //9
+//Bluetooth Definitions
+#define BLUETOOTHTX               PD2  //2
+#define BLUETOOTHRX               PD3  //3
+//Neo Pixel Stick Definitions
+#define NUM_PIXELS                8
+#define NEOPIXEL_PIN              PD4  //4
+//Ultrasonic sensor Definitions
+#define TRIGGER                   11   //
+#define ECHO                      12   // 
+//Servo Definitions
+#define SERVO                     PD5  //5
+//Button Definitions
+#define PUSH                      999  //13
+//Buzzer's Definition
+#define BEEP                      PB5  //13
